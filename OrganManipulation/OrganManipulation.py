c4e99f54d10b65adda09ebf9ee2fa69a061572e2
Rafael Palomar <rafael.palomar@ous-research.no>
2024-04-01 21:02:40 +0200

    ENH: Expose general simulation parameters in GUI

--- OrganManipulation/OrganManipulation.py
+++ OrganManipulation/OrganManipulation.py
@@ -19,8 +19,12 @@ from slicer.parameterNodeWrapper import (
     WithinRange,
 )
 
-from slicer import vtkMRMLModelNode
 from slicer import vtkMRMLIGTLConnectorNode
+from slicer import vtkMRMLMarkupsLineNode
+from slicer import vtkMRMLMarkupsROINode
+from slicer import vtkMRMLModelNode
+from slicer import vtkMRMLSequenceNode
+from slicer import vtkMRMLSequenceBrowserNode
 
 #
 # OrganManipulation
@@ -86,8 +90,69 @@ class OrganManipulationParameterNode:
     The parameters needed by module.
     """
     modelNode: vtkMRMLModelNode
+    boundaryROI: vtkMRMLMarkupsROINode
+    gravityVector: vtkMRMLMarkupsLineNode
+    gravityMagnitude: int
+    sequenceNode: vtkMRMLSequenceNode
+    sequenceBrowserNode: vtkMRMLSequenceBrowserNode
     modelNodeFileName: str
     serverPort: int
+    dt: float
+    totalSteps: int
+    currentStep: int
+
+    def getLPSModel(self):
+
+        if self.modelNode is not None:
+            transform = vtk.vtkTransform()
+            transform.Identity()  # Start with an identity matrix
+            transform.Scale(-1, -1, 1)  # Flip X and Y axes for RAS to LPS
+
+            # Apply the transformation to the input vtkPolyData
+            transform_filter = vtk.vtkTransformFilter()
+            transform_filter.SetInputData(self.modelNode.GetUnstructuredGrid())
+            transform_filter.SetTransform(transform)
+            transform_filter.Update()  # Perform the transformation
+
+            return transform_filter.GetOutput()
+
+        return None
+
+    def getBoundaryROI(self):
+
+        if self.boundaryROI is None:
+            return [0.0]*6
+
+        center = [0]*3
+        self.boundaryROI.GetCenter(center)
+        size = self.boundaryROI.GetSize()
+
+        # Calculate min and max RAS bounds from center and size
+        R_min = center[0] - size[0] / 2
+        R_max = center[0] + size[0] / 2
+        A_min = center[1] - size[1] / 2
+        A_max = center[1] + size[1] / 2
+        S_min = center[2] - size[2] / 2
+        S_max = center[2] + size[2] / 2
+
+        # Return the two opposing bounds corners
+        # First corner: (minL, minP, minS), Second corner: (maxL, maxP, maxS)
+        return [R_min, A_min, S_min, R_max, A_max, S_max]
+
+    def getGravityVector(self):
+
+        if self.gravityVector is None:
+            return [0.0]*3
+
+        import numpy as np
+
+        p1 = self.gravityVector.GetNthControlPointPosition(0)
+        p2 = self.gravityVector.GetNthControlPointPosition(1)
+        gravity_vector = np.array([p2[0]-p1[0], p2[1]-p1[1], p2[2]-p1[2]])
+        magnitude = np.linalg.norm(gravity_vector)
+        normalized_gravity_vector = gravity_vector / magnitude if magnitude != 0 else gravity_vector
+
+        return normalized_gravity_vector*self.gravityMagnitude
 
 #
 # OrganManipulationWidget
@@ -105,6 +170,23 @@ class OrganManipulationWidget(ScriptedLoadableModuleWidget, VTKObservationMixin)
         self.logic = None
         self._parameterNode = None
         self._parameterNodeGuiTag = None
+
+        # Given corners of the bounding box in LPS coordinates
+        corner2_LPS = [0, -170, 0]  # Example values, adjust as necessary
+        corner1_LPS = [48, -80, -300]  # Example values, adjust as necessary
+
+        # Calculate the center and size in LPS
+        center_LPS = [(corner1_LPS[0] + corner2_LPS[0]) / 2,
+                    (corner1_LPS[1] + corner2_LPS[1]) / 2,
+                    (corner1_LPS[2] + corner2_LPS[2]) / 2]
+
+        size_LPS = [abs(corner2_LPS[0] - corner1_LPS[0]),
+                    abs(corner2_LPS[1] - corner1_LPS[1]),
+                    abs(corner2_LPS[2] - corner1_LPS[2])]
+
+        # Convert center and size from LPS to RAS
+        center_RAS = [center_LPS[0], center_LPS[1], center_LPS[2]]
+        size_RAS = [size_LPS[0], size_LPS[1], size_LPS[2]]  # Size remains the same, as it's a magnitude
 
         # These two variables are part of a workaround to solve a race condition
         # between the parameter node update and the gui update (e.g., (1) when a dynamic
@@ -124,14 +206,14 @@ class OrganManipulationWidget(ScriptedLoadableModuleWidget, VTKObservationMixin)
         self.layout.addWidget(uiWidget)
         self.ui = slicer.util.childWidgetVariables(uiWidget)
 
+        # Create logic class. Logic implements all computations that should be possible to run
+        # in batch mode, without a graphical user interface.
+        self.logic = OrganManipulationLogic()
+
         # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
         # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
         # "setMRMLScene(vtkMRMLScene*)" slot.
         uiWidget.setMRMLScene(slicer.mrmlScene)
-
-        # Create logic class. Logic implements all computations that should be possible to run
-        # in batch mode, without a graphical user interface.
-        self.logic = OrganManipulationLogic()
 
         # Connections
 
@@ -139,7 +221,10 @@ class OrganManipulationWidget(ScriptedLoadableModuleWidget, VTKObservationMixin)
         self.addObserver(slicer.mrmlScene, slicer.mrmlScene.StartCloseEvent, self.onSceneStartClose)
         self.addObserver(slicer.mrmlScene, slicer.mrmlScene.EndCloseEvent, self.onSceneEndClose)
 
-        self.ui.startSimulationPushButton.connect("clicked()", self.onStartSimulation)
+        self.ui.startSimulationPushButton.connect("clicked()", self.logic.startSimulation)
+        self.ui.addBoundaryROIPushButton.connect("clicked()", self.logic.addBoundaryROI)
+        self.ui.addGravityVectorPushButton.connect("clicked()", self.logic.addGravityVector)
+        self.ui.addRecordingSequencePushButton.connect("clicked()", self.logic.addRecordingSequence)
 
         # Make sure parameter node is initialized (needed for module reload)
         self.initializeParameterNode()
@@ -149,9 +234,10 @@ class OrganManipulationWidget(ScriptedLoadableModuleWidget, VTKObservationMixin)
         self.removeObservers()
 
     def enter(self) -> None:
-        """Called each time the user opens this module."""
-        # Make sure parameter node exists and observed
-        self.initializeParameterNode()
+        # """Called each time the user opens this module."""
+        # # Make sure parameter node exists and observed
+        # self.initializeParameterNode()
+        pass
 
     def exit(self) -> None:
         """Called each time the user opens a different module."""
@@ -172,20 +258,20 @@ class OrganManipulationWidget(ScriptedLoadableModuleWidget, VTKObservationMixin)
             self.initializeParameterNode()
 
     def initializeParameterNode(self) -> None:
-        """Ensure parameter node exists and observed."""
         # Parameter node stores all user choices in parameter values, node selections, etc.
         # so that when the scene is saved and reloaded, these settings are restored.
 
         self.setParameterNode(self.logic.getParameterNode())
         self._parameterNode.modelNode = None
+        self._parameterNode.boundaryROI = None
+        self._parameterNode.gravityVector = None
+        self._parameterNode.sequenceNode = None
+        self._parameterNode.sequenceBrowserNode = None
+        self._parameterNode.dt = 0.001
+        self._parameterNode.currentStep = 0
+        self._parameterNode.totalSteps= 100
         self._parameterNode.serverPort = 0
         self._parameterNode.modelNodeFileName = slicer.app.temporaryPath + '/' + str(uuid.uuid4()) + ".vtk"
-
-        # # # Select default input nodes if nothing is selected yet to save a few clicks for the user
-        # if not self._parameterNode.modelNode:
-        #     firstModelNode = slicer.mrmlScene.GetFirstNodeByClass("vtkMRMLModelNode")
-        #     if firstModelNode:
-        #         self._parameterNode.modelNode = firstModelNode
 
     def setParameterNode(self, inputParameterNode: Optional[OrganManipulationParameterNode]) -> None:
         """
@@ -199,10 +285,6 @@ class OrganManipulationWidget(ScriptedLoadableModuleWidget, VTKObservationMixin)
             # Note: in the .ui file, a Qt dynamic property called "SlicerParameterName" is set on each
             # ui element that needs connection.
             self._parameterNodeGuiTag = self._parameterNode.connectGui(self.ui)
-
-    def onStartSimulation(self):
-       self.logic.startSimulation()
-
 
 #
 # OrganManipulationLogic
@@ -219,10 +301,6 @@ class OrganManipulationLogic(ScriptedLoadableModuleLogic):
     https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
     """
 
-    # SOFAIGTL_PORT = 18944
-    # SIMULATION_TPL_FILENAME = "OrganManipulation.pyscn.tpl"
-    # SIMULATION_FILENAME = "OrganManipulation.pyscn"
-
     def __init__(self) -> None:
         """Called when the logic class is instantiated. Can be used for initializing member variables."""
         ScriptedLoadableModuleLogic.__init__(self)
@@ -233,9 +311,11 @@ class OrganManipulationLogic(ScriptedLoadableModuleLogic):
         self.SOFAReceiverModelNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLModelNode')
         self.SOFAReceiverModelNode.SetName('SOFAMesh')
         self.SOFAReceiverModelNode.HideFromEditorsOn()
+        self.SOFAReceiverModelNode.Modified()
         self.SOFAReceiverModelNode.AddObserver(slicer.vtkMRMLModelNode.PolyDataModifiedEvent, self.onModelNodeModified)
         self._transformedModel = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLModelNode') #This is needed to convert RAS to LPS for SOFA
         self._transformedModel.SetName('SOFATransformedModel')
+        self._transformedModel.HideFromEditorsOn()
 
     def getParameterNode(self):
         return OrganManipulationParameterNode(super().getParameterNode())
@@ -275,14 +355,25 @@ class OrganManipulationLogic(ScriptedLoadableModuleLogic):
             logging.debug("OIGTLink server started on port " + str(_port) + " (attempt " + str(attempt) + ")")
             return _port
         else:
-            logging.debug("OIGTLink failed to start on port " + str(_port) + " (attempt )" + str(attempt) + ")")
+            logging.debug("OIGTLink failed to start on port " + str(_port) + " (attempt " + str(attempt) + ")")
             return 0
 
     def startSimulation(self) -> None:
 
-        self._transformedModel.SetAndObserveMesh(self._lpsToRas(self._parameterNode.modelNode))
+        sequenceNode = self._parameterNode.sequenceNode
+        browserNode = self._parameterNode.sequenceBrowserNode
+        modelNode = self._parameterNode.modelNode
+
+        # Synchronize and set up the sequence browser node
+        if None not in [sequenceNode, browserNode, modelNode]:
+            browserNode.AddSynchronizedSequenceNodeID(sequenceNode.GetID())
+            browserNode.AddProxyNode(modelNode, sequenceNode, False)
+            browserNode.SetRecording(sequenceNode, True)
+            browserNode.SetRecordingActive(True)
 
         import Simulations.SOFASimulationSingle as single
+
+        self._transformedModel.SetAndObserveMesh(self._parameterNode.getLPSModel())
         self._parameterNode.serverPort = self.startServer()
         self.igtlConnectorNode.RegisterIncomingMRMLNode(self.SOFAReceiverModelNode)
         if self._parameterNode.modelNode is not None and self._parameterNode.serverPort != 0:
@@ -291,30 +382,103 @@ class OrganManipulationLogic(ScriptedLoadableModuleLogic):
                 self._controller = single.SimulationController(self._parameterNode)
             self._controller.start()
 
-    def onModelNodeModified(self, caller, event):
+    def onModelNodeModified(self, caller, event) -> None:
         if self._parameterNode.modelNode.GetUnstructuredGrid() is not None:
             self._parameterNode.modelNode.GetUnstructuredGrid().SetPoints(caller.GetPolyData().GetPoints())
         elif self._parameterNode.modelNode.GetPolyData() is not None:
             self._parameterNode.modelNode.GetPolyData().SetPoints(caller.GetPolyData().GetPoints())
 
-    def _lpsToRas(self, modelNode):
+    def addBoundaryROI(self) -> None:
+        roiNode = slicer.vtkMRMLMarkupsROINode()
+        mesh = None
+        bounds = None
 
+        if self._parameterNode.modelNode is not None:
+            if self._parameterNode.modelNode.GetUnstructuredGrid() is not None:
+                mesh = self._parameterNode.modelNode.GetUnstructuredGrid()
+            elif self._parameterNode.modelNode.GetPolyData() is not None:
+                mesh = self._parameterNode.modelNode.GetPolyData()
+
+        if mesh is not None:
+            bounds = mesh.GetBounds()
+            center = [(bounds[0] + bounds[1])/2.0, (bounds[2] + bounds[3])/2.0, (bounds[4] + bounds[5])/2.0]
+            size = [abs(bounds[1] - bounds[0])/2.0, abs(bounds[3] - bounds[2])/2.0, abs(bounds[5] - bounds[4])/2.0]
+            roiNode.SetXYZ(center)
+            roiNode.SetRadiusXYZ(size[0], size[1], size[2])
+
+        roiNode = slicer.mrmlScene.AddNode(roiNode)
+        if roiNode is not None:
+            roiNode.CreateDefaultDisplayNodes()
+
+    def addGravityVector(self) -> None:
+        # Create a new line node for the gravity vector
+        gravityVector = slicer.vtkMRMLMarkupsLineNode()
+        gravityVector.SetName("Gravity")
+        mesh = None
+
+        # Check if there is a model node set in the parameter node and get its mesh
+        if self._parameterNode.modelNode is not None:
+            if self._parameterNode.modelNode.GetUnstructuredGrid() is not None:
+                mesh = self._parameterNode.modelNode.GetUnstructuredGrid()
+            elif self._parameterNode.modelNode.GetPolyData() is not None:
+                mesh = self._parameterNode.modelNode.GetPolyData()
+
+        # If a mesh is found, compute its bounding box and center
+        if mesh is not None:
+            bounds = mesh.GetBounds()
+
+            # Calculate the center of the bounding box
+            center = [(bounds[0] + bounds[1])/2.0, (bounds[2] + bounds[3])/2.0, (bounds[4] + bounds[5])/2.0]
+
+            # Calculate the vector's start and end points along the Y-axis, centered on the bounding box
+            startPoint = [center[0], bounds[2], center[2]]  # Start at the bottom of the bounding box
+            endPoint = [center[0], bounds[3], center[2]]  # End at the top of the bounding box
+
+            # Adjust the start and end points to center the vector in the bounding box
+            vectorLength = endPoint[1] - startPoint[1]
+            midPoint = startPoint[1] + vectorLength / 2
+            startPoint[1] = midPoint - vectorLength / 2
+            endPoint[1] = midPoint + vectorLength / 2
+
+            # Add control points to define the line
+            gravityVector.AddControlPoint(vtk.vtkVector3d(startPoint))
+            gravityVector.AddControlPoint(vtk.vtkVector3d(endPoint))
+
+        # Add the gravity vector line node to the scene
+        gravityVector = slicer.mrmlScene.AddNode(gravityVector)
+        if gravityVector is not None:
+            gravityVector.CreateDefaultDisplayNodes()
+
+
+    def addRecordingSequence(self) -> None:
+
+        browserNode = self._parameterNode.sequenceBrowserNode
+        modelNode = self._parameterNode.modelNode
+
+        # Ensure there is a sequence browser node; create if not present
+        if browserNode is None:
+            browserNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLSequenceBrowserNode', "SOFA Simulation")
+            browserNode.SetPlaybackActive(False)
+            browserNode.SetRecordingActive(False)
+            self._parameterNode.sequenceBrowserNode = browserNode  # Update the parameter node reference
+
+        sequenceNode = slicer.vtkMRMLSequenceNode()
+
+        # Configure the sequence node based on the proxy model node
         if modelNode is not None:
-            transform = vtk.vtkTransform()
-            transform.Identity()  # Start with an identity matrix
-            transform.Scale(-1, -1, 1)  # Flip X and Y axes for RAS to LPS
+            sequenceNodeName = modelNode.GetName() + "-Sequence"
+            sequenceNode.SetName(sequenceNodeName)
 
-            # Apply the transformation to the input vtkPolyData
-            transform_filter = vtk.vtkTransformFilter()
-            transform_filter.SetInputData(modelNode.GetUnstructuredGrid())
-            transform_filter.SetTransform(transform)
-            transform_filter.Update()  # Perform the transformation
+        # Now add the configured sequence node to the scene
+        slicer.mrmlScene.AddNode(sequenceNode)
 
-            return transform_filter.GetOutput()
+        self._parameterNode.sequenceNode = sequenceNode  # Update the parameter node reference
 
-        return None
-
-
+        # Configure index name and unit based on the master sequence node, if present
+        masterSequenceNode = browserNode.GetMasterSequenceNode()
+        if masterSequenceNode:
+            sequenceNode.SetIndexName(masterSequenceNode.GetIndexName())
+            sequenceNode.SetIndexUnit(masterSequenceNode.GetIndexUnit())
 
 #
 # OrganManipulationTest
