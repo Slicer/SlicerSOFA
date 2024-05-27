51fed827df9d779c7f3767df668d895e1d928b5e
Rafael Palomar <rafael.palomar@ous-research.no>
2024-05-27 16:58:40 +0200

    ENH: Keep default Slicer python except hook

--- SlicerSofa/SofaSimulation/__init__.py
+++ SlicerSofa/SofaSimulation/__init__.py
@@ -3,13 +3,17 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.environ['SOFA_ROOT'] = '/home/rafael/src/Slicer-SOFA/Release/inner-build/inner-build/lib/Slicer-5.7'
 
+defaultExecHook = sys.excepthook
+
 import Sofa
-import SofaRuntime
+#import SofaRuntime
 
 from .SimulationController import SimulationController
 
-__all__ = ["Sofa", "SofaRuntime", "SimulationController"]
+__all__ = ["Sofa", "SimulationController"]
 
-# Make Sofa and SofaRuntime available in the global namespace
-globals()['Sofa'] = Sofa
-globals()['SofaRuntime'] = SofaRuntime
+# # Make Sofa and SofaRuntime available in the global namespace
+# globals()['Sofa'] = Sofa
+# globals()['SofaRuntime'] = SofaRuntime
+
+sys.excepthook = defaultExecHook
