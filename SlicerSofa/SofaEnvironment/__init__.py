0401bbbeed58a70a15d9f4a8296492a0cec56e70
Rafael Palomar <rafael.palomar@ous-research.no>
2024-06-17 10:14:38 +0200

    ENH: Enable build-tree additional settings configuration

---
+++ SlicerSofa/SofaEnvironment/__init__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+if os.path.isdir(script_dir + '/../../../../../Sofa-build'): # Build tree
+    os.environ['SOFA_ROOT'] = script_dir + '/../../../../../Sofa-build'
+else: # Install tree
+    # Sofa does not allow much configurability of the install tree, therefore it is needed
+    # to add extra python paths.
+    sys.path.append(script_dir + '/../../../../plugins/SofaPython3/lib/python3/site-packages')
+    sys.path.append(script_dir + '/../../../../plugins/STLIB/lib/python3/site-packages')
+    os.environ['SOFA_ROOT'] = script_dir + '/../../../../'
+
+# Sofa, by default, will capture the exception handling. This is a workaround
+# to keep the exception handling in PythonSlicer
+defaultExecHook = sys.excepthook
+
+import Sofa
+import SofaRuntime
+
+sys.excepthook = defaultExecHook
+
+__all__ = ["Sofa", "SofaRuntime"]
