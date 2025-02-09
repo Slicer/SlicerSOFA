d75491cec4d998de5100da43408db4582a524584
Rafael Palomar <RafaelPalomar@users.noreply.github.com>
2025-02-09 18:11:53 +0100

    Revert "BUG: Fix path assembly for SlicerSOFA (#43)"
    This reverts commit 1e45a2221fbaf3418f00190d7d05ebd23d111a2c.

--- SlicerSofa/SofaEnvironment/__init__.py
+++ SlicerSofa/SofaEnvironment/__init__.py
@@ -8,11 +8,8 @@ if os.path.isdir(script_dir + '/../../../../../Sofa-build'): # Build tree
 else: # Install tree
     # Sofa does not allow much configurability of the install tree, therefore it is needed
     # to add extra python paths.
-    sys.path = [
-        script_dir + '/../../../../plugins/SofaPython3/lib/python3/site-packages',
-        script_dir + '/../../../../plugins/STLIB/lib/python3/site-packages'
-    ] + sys.path
-
+    sys.path.append(script_dir + '/../../../../plugins/SofaPython3/lib/python3/site-packages')
+    sys.path.append(script_dir + '/../../../../plugins/STLIB/lib/python3/site-packages')
     os.environ['SOFA_ROOT'] = script_dir + '/../../../../'
 
 # Sofa, by default, will capture the exception handling. This is a workaround
