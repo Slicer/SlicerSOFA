d8a3075cfcdd3ea39d657bc37a6fec7a32dca4a5
Rafael Palomar <rafpal@IVS-Aud-Mac-mini.local>
2025-04-03 15:46:48 +0200

    ENH: Fix installation of sofa libraries
    ENH: Fix MacOS paths
    
    WIP: Simplify installation of Sofa libraries
    
    WIP: Include default plugins list

--- SlicerSofa/SofaEnvironment/__init__.py
+++ SlicerSofa/SofaEnvironment/__init__.py
@@ -2,18 +2,11 @@ import os
 import sys
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path = [
+    script_dir + '/../../Sofa/lib/python3/site-packages',
+] + sys.path
 
-if os.path.isdir(script_dir + '/../../../../../Sofa-build'): # Build tree
-    os.environ['SOFA_ROOT'] = script_dir + '/../../../../../Sofa-build'
-else: # Install tree
-    # Sofa does not allow much configurability of the install tree, therefore it is needed
-    # to add extra python paths.
-    sys.path = [
-        script_dir + '/../../../../plugins/SofaPython3/lib/python3/site-packages',
-        script_dir + '/../../../../plugins/STLIB/lib/python3/site-packages'
-    ] + sys.path
-
-    os.environ['SOFA_ROOT'] = script_dir + '/../../../../'
+os.environ['SOFA_ROOT'] = script_dir + '/../../Sofa'
 
 # Sofa, by default, will capture the exception handling. This is a workaround
 # to keep the exception handling in PythonSlicer
