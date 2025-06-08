9264de1c66f4307eb3e3c9f992940d83a6dc1ec5
Rafael Palomar <rafael.palomar@ous-research.no>
2025-06-08 18:25:13 +0200

    ENH: Fix SOFA packaging

--- SlicerSofa/SofaEnvironment/__init__.py
+++ SlicerSofa/SofaEnvironment/__init__.py
@@ -3,17 +3,12 @@ import sys
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-if os.path.isdir(script_dir + '/../../../../../Sofa-build'): # Build tree
+if os.path.isdir(script_dir + '/../../python3'): #Install-tree
+    os.environ['SOFA_ROOT'] = script_dir + '/../..'
+    sys.path = [script_dir + '/../../python3/site-packages'] + sys.path
+else:                                         #Build-tree
     os.environ['SOFA_ROOT'] = script_dir + '/../../../../../Sofa-build'
-else: # Install tree
-    # Sofa does not allow much configurability of the install tree, therefore it is needed
-    # to add extra python paths.
-    sys.path = [
-        script_dir + '/../../../../plugins/SofaPython3/lib/python3/site-packages',
-        script_dir + '/../../../../plugins/STLIB/lib/python3/site-packages'
-    ] + sys.path
-
-    os.environ['SOFA_ROOT'] = script_dir + '/../../../../'
+    sys.path = [script_dir + '/../../../../../Sofa-build/lib/python3/site-packages'] + sys.path
 
 # Sofa, by default, will capture the exception handling. This is a workaround
 # to keep the exception handling in PythonSlicer
